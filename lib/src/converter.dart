25b8dd3f4857d9e663f3e742380763cf35c0c3bf
benjamin <benjamin.chung@cognixus.com>
2021-11-05 00:22:03 +0800

    Fix fromJson after null safety migration
    For null safety, we need to use firstWhereOrNull from collection. It does not like it if firstWhere with orElse returning null is used.

--- lib/src/converter.dart
+++ lib/src/converter.dart
@@ -1,11 +1,13 @@
+import 'package:collection/collection.dart';
+
 abstract class Converter<T> {
   Map<String, dynamic>? toMap(T value);
   T? fromMap(Map<String, dynamic> map);
 
   T? enumFromJson<T>(List<T?> values, String? json) => json != null
-      ? values.firstWhere(
+      ? values.firstWhereOrNull(
           (it) => enumToJson(it)?.toLowerCase() == json.toLowerCase(),
-          orElse: () => null)
+        )
       : null;
 
   String? enumToJson<T>(T value) =>
